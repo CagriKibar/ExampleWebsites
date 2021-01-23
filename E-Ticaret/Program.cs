261cc34678457245eda611dba4f18e60bb22a411
CagriKibar <helloCagri+CagriKibar@users.noreply.github.com>
2021-01-23 15:03:34 +0300

    Proje dosyası ekle.

---
+++ E-Ticaret/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E_Ticaret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
